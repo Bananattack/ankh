2f8dc0894bbd6847cc76d7867a8df89a88141fc7
speveril <speveril@gmail.com>
2012-08-10 23:50:06 -0700

    Take the dumb .src off of the namespace. Why was it there?

--- Defenetron/src/Main.cs
+++ Defenetron/src/Main.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Defenetron.src {
+namespace Defenetron {
     class GameApp {
         static int Main(string[] args) {
             return 0;
