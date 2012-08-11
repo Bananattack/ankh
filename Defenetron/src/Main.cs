e7ad462c4281a1b8310e5f6f58e3292e4808ae05
speveril <speveril@gmail.com>
2012-08-10 23:19:28 -0700

    Add program entry point, fix the default namespace for the project.

---
+++ Defenetron/src/Main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Defenetron.src {
+    class GameApp {
+        static void Main(string[] args) {
+            return;
+        }
+    }
+}
