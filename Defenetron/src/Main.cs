da7de41b7b6664c4139662e3fc95cc14fd5ec6e2
speveril <speveril@gmail.com>
2012-08-11 12:46:43 -0700

    Make it a windows app.

--- Defenetron/src/Main.cs
+++ Defenetron/src/Main.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace Defenetron {
-    class GameApp {
-        static int Main(string[] args) {
-            return 0;
+    class GameApp:Form {
+        GameApp() {
+
+        }
+
+        static void Main(string[] args) {
+            GameApp app = new GameApp();
+            Application.Run(app);
         }
     }
 }
